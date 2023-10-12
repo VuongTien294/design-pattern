23ddb77c65f296c9142490990b09b6cb345f5360
Vuong Tien <vuongtien2942000@gmail.com>
2023-10-12 14:58:17 +0700

    o

--- src/CreationalPatterns/BuiderDesignPattern/Main.java
+++ src/CreationalPatterns/BuiderDesignPattern/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Person person = new Person.PersonBuilder()
                 .id(666)
                 .name("Levent")
-                .surname("Divilioglu")
+                .surname("Surname")
                 .setOccupied(true)
                 .build();
 
